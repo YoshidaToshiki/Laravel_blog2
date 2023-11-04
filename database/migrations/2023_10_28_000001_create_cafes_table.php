660ed2ea3419b559e95b9eb58deb97b153b6052b
YoshidaToshiki <toshiki1339@yahoo.co.jp>
2023-11-05 08:16:19 +0900

    first commit

---
+++ database/migrations/2023_10_28_000001_create_cafes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cafes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('area_id')->constrained('areas');
+            $table->string('address');
+            $table->text('access');
+            $table->text('image');
+            $table->tinyInteger('breadth');
+            $table->boolean('management_flg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cafes');
+    }
+};
---
+++ database/migrations/2023_10_28_000003_create_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cafe_id')->constrained('cafes');
+            $table->text('body');
+            $table->text('image');
+            $table->tinyInteger('recommends');
+            $table->boolean('limited_flg');
+            $table->timestamp('limited_time')->nullable();
+            $table->boolean('take_out_flg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+};
